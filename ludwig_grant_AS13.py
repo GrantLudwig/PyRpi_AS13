b3d4feedf280e1cc3cc180ae23266455d8271b8f
GrantLudwig <grantludwig97@gmail.com>
2019-10-25 12:35:10 -0700

    initial commit

---
+++ ludwig_grant_AS13.py
@@ -0,0 +1,8 @@
+# File Name: ludwig_grant_AS13.py
+# File Path: /home/ludwigg/Python/PyRpi_AS13/ludwig_grant_AS13.py
+# Run Command: sudo python3 /home/ludwigg/Python/PyRpi_AS13/ludwig_grant_AS13.py
+
+# Grant Ludwig
+# 10/28/2019
+# AS.13
+# Sort 3 numbers
